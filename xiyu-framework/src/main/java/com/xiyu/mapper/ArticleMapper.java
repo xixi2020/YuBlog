62a7c3129a1633956b780448cae831aa7e4bfeb7
xixi2020 <xi19990119@qq.com>
2024-07-24 23:19:30 +0800

    博客系统框架搭建与调试

---
+++ xiyu-framework/src/main/java/com/xiyu/mapper/ArticleMapper.java
@@ -0,0 +1,7 @@
+package com.xiyu.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.xiyu.domain.entity.Article;
+
+public interface ArticleMapper extends BaseMapper<Article> {
+}
